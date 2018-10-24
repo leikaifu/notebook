9ef31f49e8f4443028effcb7e5b9099d19595394
1269304645@qq.com <you@example.com>
2018-10-24 15:49:41 +0800

    22

--- src/com/itheima/AccountDaoImpl.java
+++ src/com/itheima/AccountDaoImpl.java
@@ -8,7 +8,7 @@ public class AccountDaoImpl extends JdbcDaoSupport implements AccountDao {
     public void Outmoney(String from, double money) {
         this.getJdbcTemplate().update("UPDATE account set money=money - ? WHERE name=? ",money,from);
     }
-
+    
     @Override
     public void Inmoney(String to, double money) {
         this.getJdbcTemplate().update("UPDATE account set money=money+? WHERE name=?",money,to);
